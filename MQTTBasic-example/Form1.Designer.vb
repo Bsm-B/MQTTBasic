b5c587490422b302857281e28e9322bfd96bea51
unknown <bsmbousselmi@gmail.com>
2020-05-04 22:59:20 -0700

    Beta

--- MQTTBasic-example/Form1.Designer.vb
+++ MQTTBasic-example/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,7 +20,7 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
@@ -30,14 +30,12 @@ Partial Class Form1
         Me.Label6 = New System.Windows.Forms.Label()
         Me.Label7 = New System.Windows.Forms.Label()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
         Me.TextBox3 = New System.Windows.Forms.TextBox()
         Me.TextBox4 = New System.Windows.Forms.TextBox()
         Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
         Me.RichTextBox2 = New System.Windows.Forms.RichTextBox()
         Me.ComboBox1 = New System.Windows.Forms.ComboBox()
         Me.ComboBox2 = New System.Windows.Forms.ComboBox()
-        Me.Label8 = New System.Windows.Forms.Label()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.Button3 = New System.Windows.Forms.Button()
@@ -47,6 +45,7 @@ Partial Class Form1
         Me.GroupBox3 = New System.Windows.Forms.GroupBox()
         Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
         Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.Button5 = New System.Windows.Forms.Button()
         Me.GroupBox1.SuspendLayout()
         Me.GroupBox2.SuspendLayout()
         Me.GroupBox3.SuspendLayout()
@@ -120,15 +119,9 @@ Partial Class Form1
         '
         Me.TextBox1.Location = New System.Drawing.Point(66, 24)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(261, 22)
+        Me.TextBox1.Size = New System.Drawing.Size(373, 22)
         Me.TextBox1.TabIndex = 7
-        '
-        'TextBox2
-        '
-        Me.TextBox2.Location = New System.Drawing.Point(375, 24)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(64, 22)
-        Me.TextBox2.TabIndex = 8
+        Me.TextBox1.Text = "broker.hivemq.com"
         '
         'TextBox3
         '
@@ -148,7 +141,7 @@ Partial Class Form1
         '
         Me.RichTextBox1.Location = New System.Drawing.Point(6, 165)
         Me.RichTextBox1.Name = "RichTextBox1"
-        Me.RichTextBox1.Size = New System.Drawing.Size(433, 106)
+        Me.RichTextBox1.Size = New System.Drawing.Size(433, 128)
         Me.RichTextBox1.TabIndex = 11
         Me.RichTextBox1.Text = ""
         '
@@ -162,32 +155,27 @@ Partial Class Form1
         '
         'ComboBox1
         '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
         Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"At most once (0)", "At least once (1)", "Exactly once (2)"})
         Me.ComboBox1.Location = New System.Drawing.Point(9, 97)
         Me.ComboBox1.Name = "ComboBox1"
-        Me.ComboBox1.Size = New System.Drawing.Size(106, 24)
+        Me.ComboBox1.Size = New System.Drawing.Size(150, 24)
         Me.ComboBox1.TabIndex = 13
         '
         'ComboBox2
         '
+        Me.ComboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
         Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Items.AddRange(New Object() {"At most once (0)", "At least once (1)", "Exactly once (2)"})
         Me.ComboBox2.Location = New System.Drawing.Point(6, 110)
         Me.ComboBox2.Name = "ComboBox2"
-        Me.ComboBox2.Size = New System.Drawing.Size(106, 24)
+        Me.ComboBox2.Size = New System.Drawing.Size(150, 24)
         Me.ComboBox2.TabIndex = 14
-        '
-        'Label8
-        '
-        Me.Label8.AutoSize = True
-        Me.Label8.Location = New System.Drawing.Point(333, 27)
-        Me.Label8.Name = "Label8"
-        Me.Label8.Size = New System.Drawing.Size(38, 17)
-        Me.Label8.TabIndex = 15
-        Me.Label8.Text = "Port:"
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(310, 75)
+        Me.Button1.Location = New System.Drawing.Point(310, 52)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(132, 32)
         Me.Button1.TabIndex = 16
@@ -205,7 +193,7 @@ Partial Class Form1
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(310, 277)
+        Me.Button3.Location = New System.Drawing.Point(310, 299)
         Me.Button3.Name = "Button3"
         Me.Button3.Size = New System.Drawing.Size(129, 36)
         Me.Button3.TabIndex = 18
@@ -214,7 +202,7 @@ Partial Class Form1
         '
         'Button4
         '
-        Me.Button4.Location = New System.Drawing.Point(203, 75)
+        Me.Button4.Location = New System.Drawing.Point(204, 52)
         Me.Button4.Name = "Button4"
         Me.Button4.Size = New System.Drawing.Size(100, 32)
         Me.Button4.TabIndex = 19
@@ -230,9 +218,9 @@ Partial Class Form1
         Me.GroupBox1.Controls.Add(Me.TextBox4)
         Me.GroupBox1.Controls.Add(Me.Label6)
         Me.GroupBox1.Controls.Add(Me.ComboBox2)
-        Me.GroupBox1.Location = New System.Drawing.Point(12, 145)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 123)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(448, 319)
+        Me.GroupBox1.Size = New System.Drawing.Size(448, 341)
         Me.GroupBox1.TabIndex = 21
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "Publish"
@@ -241,19 +229,18 @@ Partial Class Form1
         '
         Me.GroupBox2.Controls.Add(Me.Label1)
         Me.GroupBox2.Controls.Add(Me.TextBox1)
-        Me.GroupBox2.Controls.Add(Me.Label8)
         Me.GroupBox2.Controls.Add(Me.Button4)
-        Me.GroupBox2.Controls.Add(Me.TextBox2)
         Me.GroupBox2.Controls.Add(Me.Button1)
         Me.GroupBox2.Location = New System.Drawing.Point(12, 26)
         Me.GroupBox2.Name = "GroupBox2"
-        Me.GroupBox2.Size = New System.Drawing.Size(448, 113)
+        Me.GroupBox2.Size = New System.Drawing.Size(448, 94)
         Me.GroupBox2.TabIndex = 22
         Me.GroupBox2.TabStop = False
         Me.GroupBox2.Text = "Config"
         '
         'GroupBox3
         '
+        Me.GroupBox3.Controls.Add(Me.Button5)
         Me.GroupBox3.Controls.Add(Me.Label2)
         Me.GroupBox3.Controls.Add(Me.TextBox3)
         Me.GroupBox3.Controls.Add(Me.Label5)
@@ -261,9 +248,9 @@ Partial Class Form1
         Me.GroupBox3.Controls.Add(Me.Button2)
         Me.GroupBox3.Controls.Add(Me.Label7)
         Me.GroupBox3.Controls.Add(Me.RichTextBox2)
-        Me.GroupBox3.Location = New System.Drawing.Point(466, 26)
+        Me.GroupBox3.Location = New System.Drawing.Point(469, 26)
         Me.GroupBox3.Name = "GroupBox3"
-        Me.GroupBox3.Size = New System.Drawing.Size(468, 438)
+        Me.GroupBox3.Size = New System.Drawing.Size(470, 438)
         Me.GroupBox3.TabIndex = 23
         Me.GroupBox3.TabStop = False
         Me.GroupBox3.Text = "Subscribe"
@@ -274,7 +261,7 @@ Partial Class Form1
         Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1})
         Me.StatusStrip1.Location = New System.Drawing.Point(0, 501)
         Me.StatusStrip1.Name = "StatusStrip1"
-        Me.StatusStrip1.Size = New System.Drawing.Size(948, 26)
+        Me.StatusStrip1.Size = New System.Drawing.Size(947, 26)
         Me.StatusStrip1.TabIndex = 24
         Me.StatusStrip1.Text = "StatusStrip1"
         '
@@ -284,11 +271,20 @@ Partial Class Form1
         Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(49, 20)
         Me.ToolStripStatusLabel1.Text = "Status"
         '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(355, 118)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(100, 29)
+        Me.Button5.TabIndex = 18
+        Me.Button5.Text = "Clear"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(948, 527)
+        Me.ClientSize = New System.Drawing.Size(947, 527)
         Me.Controls.Add(Me.StatusStrip1)
         Me.Controls.Add(Me.GroupBox3)
         Me.Controls.Add(Me.GroupBox2)
@@ -321,14 +317,12 @@ Partial Class Form1
     Friend WithEvents Label6 As Label
     Friend WithEvents Label7 As Label
     Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents TextBox2 As TextBox
     Friend WithEvents TextBox3 As TextBox
     Friend WithEvents TextBox4 As TextBox
     Friend WithEvents RichTextBox1 As RichTextBox
     Friend WithEvents RichTextBox2 As RichTextBox
     Friend WithEvents ComboBox1 As ComboBox
     Friend WithEvents ComboBox2 As ComboBox
-    Friend WithEvents Label8 As Label
     Friend WithEvents Button1 As Button
     Friend WithEvents Button2 As Button
     Friend WithEvents Button3 As Button
@@ -338,4 +332,5 @@ Partial Class Form1
     Friend WithEvents GroupBox3 As GroupBox
     Friend WithEvents StatusStrip1 As StatusStrip
     Friend WithEvents ToolStripStatusLabel1 As ToolStripStatusLabel
+    Friend WithEvents Button5 As Button
 End Class
--- MQTTBasic-example/Form1.vb
+++ MQTTBasic-example/Form1.vb
@@ -1,3 +1,135 @@
-﻿Public Class Form1
+﻿Imports uPLibrary.Networking.M2Mqtt
+Imports uPLibrary.Networking.M2Mqtt.Messages
+Imports System.Text
+Imports System.Threading
 
+
+
+Public Class Form1
+
+    Dim client As MqttClient
+    Dim Msg As StringBuilder = New StringBuilder(4096)
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If (TextBox1.Text.Length <> 0) Then
+
+
+            Try
+                client = New MqttClient(TextBox1.Text)
+
+                Dim clientId As String = Guid.NewGuid().ToString()
+
+                AddHandler client.MqttMsgPublishReceived, AddressOf Client_MqttMsgPublishReceived
+                client.Connect(clientId)
+
+                If client.IsConnected Then
+                    ComboBox1.SelectedIndex = 0
+                    ComboBox2.SelectedIndex = 0
+
+                    ToolStripStatusLabel1.Text = "Connected to " + "'" + TextBox1.Text + "'"
+                Else
+                    ToolStripStatusLabel1.Text = "Disconnected"
+                End If
+
+            Catch ex As Exception
+
+                ToolStripStatusLabel1.Text = "Error"
+                MsgBox(ex.Message(), MsgBoxStyle.Critical)
+            End Try
+        Else
+            ToolStripStatusLabel1.Text = "Please enter a valid Broker address "
+
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If (client IsNot Nothing AndAlso client.IsConnected()) Then
+            client.Disconnect()
+            ToolStripStatusLabel1.Text = "Disconnected"
+        Else
+            ToolStripStatusLabel1.Text = "Error"
+        End If
+    End Sub
+
+    Private Sub Client_MqttMsgPublishReceived(ByVal sender As Object, ByVal e As MqttMsgPublishEventArgs)
+        Msg.AppendLine()
+        Msg.Append("[" + TimeOfDay.ToString("h:mm:ss tt") + "] Topic: " + e.Topic.ToString() + ", Len: " + e.Message.Length.ToString() + ", Qos: " + e.QosLevel.ToString())
+        Msg.AppendLine()
+        Msg.Append("Msg : " + Encoding.Default.GetString(e.Message))
+        Msg.AppendLine()
+
+        SetText(Msg.ToString)
+
+    End Sub
+
+    Delegate Sub SetTextCallback(newString As String)
+    Private Sub SetText(ByVal newString As String)
+        ' Calling from another thread? -> Use delegate
+        If Me.RichTextBox2.InvokeRequired Then
+            Dim d As New SetTextCallback(AddressOf SetText)
+            ' Execute delegate in the UI thread, pass args as an array
+            Me.Invoke(d, New Object() {newString})
+        Else ' Same thread, assign string to the textbox
+            Me.RichTextBox2.Text = newString
+        End If
+    End Sub
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If (client IsNot Nothing AndAlso client.IsConnected()) Then
+            If (TextBox3.Text.Length <> 0) Then
+
+                Try
+                    Dim Topic() As String = {TextBox3.Text}
+                    Dim Qos() As Byte = {ComboBox1.SelectedIndex}
+                    client.Subscribe(Topic, Qos)
+                    ToolStripStatusLabel1.Text = "Subscribe to " + "{" + TextBox3.Text + "}"
+
+                Catch ex As Exception
+                    ToolStripStatusLabel1.Text = "Error"
+                    MsgBox(ex.Message, MsgBoxStyle.Critical)
+
+                End Try
+
+
+            Else
+                ToolStripStatusLabel1.Text = "Please enter a valid topic "
+            End If
+
+
+        Else
+            ToolStripStatusLabel1.Text = "Disconnected !! subscription procedure is not valid"
+        End If
+
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If (client IsNot Nothing AndAlso client.IsConnected()) Then
+            If (TextBox4.Text.Length <> 0) Then
+                Try
+                    Dim Qos As Byte = ComboBox2.SelectedIndex
+                    client.Publish(TextBox4.Text, Encoding.Default.GetBytes(RichTextBox1.Text), Qos, False)
+                    ToolStripStatusLabel1.Text = "Publish to " + "{" + TextBox4.Text + "}"
+                Catch ex As Exception
+                    ToolStripStatusLabel1.Text = "Error"
+                    MsgBox(ex.Message, MsgBoxStyle.Critical)
+                End Try
+
+
+            Else
+
+                ToolStripStatusLabel1.Text = "Please enter a valid topic "
+            End If
+        Else
+            ToolStripStatusLabel1.Text = "Disconnected !! Publish procedure is not valid"
+        End If
+
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Msg.Clear()
+        RichTextBox2.Clear()
+    End Sub
 End Class
